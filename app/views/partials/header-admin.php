3c659f9ff3cb5476c3c0eabfe2b23e9cb7d109e9
jer <mjlerba@gmail.com>
2026-07-27 15:22:48 +0200

    feat:update header-admin.php

--- app/views/partials/header-admin.php
+++ app/views/partials/header-admin.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="/assets/css/custom.css" rel="stylesheet">
     <link href="/assets/css/admin.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 </head>
 <body>
 <div id="theme-toggle" class="theme-toggle">🌙</div>
