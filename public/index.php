8fb872e5c22dc20a3c67959f548365bc18b6bdaa
jer <mjlerba@gmail.com>
2026-08-27 11:08:29 +0200

    Clean up temporary debugging code before deployment

--- public/index.php
+++ public/index.php
@@ -4,9 +4,9 @@ date_default_timezone_set('Africa/Lubumbashi');
 session_start();
 
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
